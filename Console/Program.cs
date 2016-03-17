760a816b58bef96859b5631004669f3abdeaba37
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-03-17 15:56:27 -0500

    Updated projects for latest Sharp.Xmpp

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,11 +1,7 @@
-﻿using S22.Xmpp.Client;
-using S22.Xmpp;
+﻿using Sharp.Xmpp;
+using Sharp.Xmpp.Client;
+using Sharp.Xmpp.Im;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using S22.Xmpp.Im;
 
 namespace XmppConsole
 {
